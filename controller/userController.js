baa22b96b072a09b3712947549c339124c27bd68
Matías <crosserain@gmail.com>
2023-07-29 16:36:51 -0300

    Function added, Create new user with POST method

--- controller/userController.js
+++ controller/userController.js
@@ -6,12 +6,24 @@ const userDB = require("model/userM.js");
 
 app.get("/api/users", (req, res) => {
     userDB.getAll((err, rows) => {
-        if(err){
+        if (err) {
             res.status(500).send(err);
             throw err;
-        }
-        else{
+        } else {
             res.send(rows);
+        }
+    });
+});
+
+app.post("/api/users", (req, res) => {
+    console.log(req.body);
+    const newUser = Object.values(req.body);
+    userDB.create(newUser, (err) => {
+        if (err) {
+            res.status(500).send(err);
+            throw err;
+        } else {
+            res.send(`Se agregó el usuario con el mail: ${newUser[0]}`);
         }
     });
 });
--- model/userM.js
+++ model/userM.js
@@ -1,23 +1,31 @@
 require("rootpath")();
 const mysql = require("mysql");
 const config = require("config.json");
+const { use } = require("../controller/personController");
 
 const connection = mysql.createConnection(config.database);
-connection.connect(err => {
-    if(err){
+connection.connect((err) => {
+    if (err) {
         console.log(err.stack);
         return;
     }
     console.log(`Conectado con la id: ${connection.threadId}`);
-})
+});
 
 const userDB = {};
 
-userDB.getAll = function(callBackUsers){
+userDB.getAll = function (callBackUsers) {
     const usersQuery = "SELECT * FROM usuario;";
     connection.query(usersQuery, (err, rows) => {
         callBackUsers(err, rows);
     });
 };
 
+userDB.create = function (newUser, callBackCreate) {
+    const createQuery =
+        "INSERT INTO usuario (mail, nickname, password) VALUES (?,?,?);";
+    connection.query(createQuery, newUser, (err) => {
+        callBackCreate(err, newUser);
+    });
+};
 module.exports = userDB;
